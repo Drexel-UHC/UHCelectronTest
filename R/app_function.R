2aaadfa73a012906e77101ed6257a8ec82056d0d
rl627 <rl627@drexel.edu>
2021-08-31 14:07:19 -0400

    Initial commit

---
+++ R/app_function.R
@@ -0,0 +1,12 @@
+#' A Cat Function
+#'
+#' This function allows you to run the shiny appts.
+#' @param
+#' @keywords shiny
+#' @export
+#' @examples
+#' run_UHC_app()
+
+run_UHC_app <- function(){
+  print("RUN APP")
+}
